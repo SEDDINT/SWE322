aeca41b1911d990795e202c7533f0fe5a66a28bf
SalahEddin Thabet <108817342+SEDDINT@users.noreply.github.com>
2023-02-07 14:51:10 +0300

    Create 03_lab.php

---
+++ Lab work/03_lab/03_lab.php
@@ -0,0 +1,26 @@
+<?php
+
+function add($x, $y)
+{
+    return ($x + $y);
+}
+function substract($x, $y)
+{
+    return ($x - $y);
+}
+function multiply($x, $y)
+{
+    return ($x * $y);
+}
+function divide($x, $y)
+{
+    return ($x / $y);
+}
+
+echo add(10, 2);
+echo "<br>";
+echo substract(10, 2);
+echo "<br>";
+echo multiply(10, 2);
+echo "<br>";
+echo divide(10, 2);
