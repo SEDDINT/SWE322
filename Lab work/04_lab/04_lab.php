c591ca114c6f2bd5797954fe79abcaccbc12b9f6
SalahEddin Thabet <108817342+SEDDINT@users.noreply.github.com>
2023-02-13 20:45:10 +0300

    Update 04_lab.php

--- Lab work/04_lab/04_lab.php
+++ Lab work/04_lab/04_lab.php
@@ -10,7 +10,6 @@ for ($x = 0; $x < $arr_Length; $x++) {
 }
 
 echo "<hr>";
-echo "<br>";
 
 $corse = array(
     "SWE301" => "requirement enginering",
@@ -19,16 +18,25 @@ $corse = array(
 );
 
 echo "<h1> corces I have taken: </h1>";
-foreach ($corse as $c => $c_value) {
-    echo "<h3> code = " . $c . ", course name = " . $c_value . ".</h3>";
-}
+echo "<table border=3>";
+echo "<tr>";
+echo "<th>CODE</th>";
+echo "<th>Cource name</th>";
 
+foreach ($corse as $c => $c_value) {
+    echo "<tr>";
+    echo "<td> $c </td>";
+    echo "<td> $c_value </td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "<hr>";
+echo "<br>";
 $info = array(
     array("Labtop", "Asus", "MSI"),
     array("CPU", "AMD", "Intel"),
     array("GPU", "AMD", "Nvidia")
 );
-echo "<hr>";
-echo "<br>";
+
 
 echo "I am looking for " . $info[0][1] . " " . $info[0][0] . " with " . $info[1][1] . " " . $info[1][0] . " and " . $info[2][1] . " " . $info[2][2] . ".";
